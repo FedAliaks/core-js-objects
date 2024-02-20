3c37a641b662c0cc4f9929386ae3820f6303db8f
Aliaksandr Fedukovich <1632637@mail.ru>
2024-02-20 16:35:46 +0300

    feat: add solution

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -34,8 +34,20 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const answer = {};
+  objects.forEach((object) => {
+    Object.entries(object).forEach((arr) => {
+      const [key, value] = arr;
+      if (key in answer) {
+        answer[key] += value;
+      } else {
+        answer[key] = value;
+      }
+    });
+  });
+
+  return answer;
 }
 
 /**
